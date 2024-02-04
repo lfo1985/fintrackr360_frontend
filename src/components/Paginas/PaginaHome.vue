e430c522ced9e3607b42d7f3bf2fa26e3e98895b
lfo1985 <lfo1985@gmail.com>
2024-02-04 11:34:40 -0300

    Correção da variável baseURL da configuração

--- src/components/Paginas/PaginaHome.vue
+++ src/components/Paginas/PaginaHome.vue
@@ -38,6 +38,7 @@ export default {
 	},
 	mounted(){
 		this.buscar();
+		console.log(location.hostname);
 	}
 }
 </script>
---
+++ src/config.js
@@ -0,0 +1,5 @@
+export default {
+    titulo: 'FinTracker360',
+    versao: 'BETA',
+    baseURL: location.hostname == 'localhost' ? 'http://localhost:8000/api/' : 'http://54.91.130.29/data/api/',
+}

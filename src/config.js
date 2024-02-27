84a0941f5417f2ff983fd1a2d33f5a2790027eba
lfo1985 <lfo1985@gmail.com>
2024-02-27 18:21:30 -0300

    Alteração da versão do sistema

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
     titulo: 'FinTracker360',
-    versao: 'BETA',
+    versao: '1.0',
     baseURL: location.hostname == 'localhost' ? 'http://localhost:8000/api/' : 'http://54.91.130.29/data-ft360/api/',
 }

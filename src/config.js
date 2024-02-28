9a67530985a110ab6acdc321896783dcaf497673
lfo1985 <lfo1985@gmail.com>
2024-02-28 17:33:04 -0300

    Ajustando o ip da api

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
     titulo: 'FinTracker360',
     versao: '1.0',
-    baseURL: location.hostname == 'localhost' ? 'http://localhost:8000/api/' : 'http://54.91.130.29/data-ft360/api/',
+    baseURL: location.hostname == 'localhost' ? 'http://localhost:8000/api/' : 'http://44.201.158.226/data-ft360/api/',
 }
